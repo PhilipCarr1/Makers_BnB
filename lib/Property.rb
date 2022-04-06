79a5fee78414d0c14fc61ceba2194091d3a6852f
Jake Dance <jake.dance1991@gmail.com>
2022-04-06 10:55:08 +0100

    Added Propery class in lib folder and correlating spec in spec folder - also merged feature and features folder

---
+++ lib/Property.rb
@@ -0,0 +1,11 @@
+require 'pg'
+
+class Property
+  def self.open_connection
+    if ENV['ENVIRONMENT'] == 'test'
+    connection = PG.connect(dbname: 'makersbnb_test')
+    else
+    connection = PG.connect(dbname: 'makersbnb')
+    end
+  end
+end
