4c75ad4f5d2a70a468f1097aace1645c1d14e37a
Eleanor Chamberlain-Stoltzfus <eleanor.stoltzfus@gmail.com>
2022-04-06 11:23:31 +0100

    .create unit test, .create method and adjustment to related app.rb POST method

--- spec/Property_spec.rb
+++ spec/Property_spec.rb
@@ -1,1 +1,17 @@
 require "Property"
+
+require 'database_helpers'
+
+describe '.create' do
+  it 'creates a new property' do
+    makersbnb = Property.create(property_name: "Test Name", description: "Test description", price: 90.40, host_name: "Test host name")
+    persisted_data = persisted_data(id: makersbnb.id)
+
+    expect(makersbnb).to be_a Property
+    expect(makersbnb.id).to eq persisted_data['id']
+    expect(makersbnb.property_name).to eq "Test Name"
+    expect(makersbnb.description).to eq "Test description"
+    expect(makersbnb.price).to eq 90.40
+    expect(makersbnb.host_name).to eq "Test host name"
+  end
+end
