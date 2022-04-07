94c10ee7f1584a0106c49ba6fb3eee2fe2739511
Jake Dance <jake.dance1991@gmail.com>
2022-04-07 16:18:38 +0100

    added a added booking page

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -1,6 +1,6 @@
  feature 'Viewing welcome page' do
   scenario 'visiting / page' do
     visit('/')
-    expect(page).to have_content "Welcome to Makers BnB"
+    expect(page).to have_content "Welcome to Bee N Bees.com!"
   end
  end
